5981f0dce5ca50b7c4d9fecde4012baa9a708391
tolaseadegbite <tolasekelvin@gmail.com>
2024-01-13 22:11:31 +0100

    Style sign up and account settings page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authenticate_user!
 
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :surname, :location, :ywam_base, :bio, :admin])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :surname, :location, :ywam_base, :bio, :admin])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :surname, :location, :ywam_base, :bio, :admin])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :firstname, :surname, :location, :ywam_base, :bio, :admin])
     end
 end
